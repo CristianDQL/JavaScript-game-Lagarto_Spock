233de235d347b7192dfa22d21c7dc58d1b194207
Cristian.Quiceno <cristian.qprog@gmail.com>
2023-10-12 17:27:23 +0200

    add fondo y hoover

--- SRC/JS/code.js
+++ SRC/JS/code.js
@@ -11,7 +11,9 @@ let player;
 let boton;
 
 let main;
+let salon;
 
+let imagenes;
 let piedra;
 let papel;
 let tijera;
@@ -37,7 +39,11 @@ function elementAccess (){
 
     boton.disabled = true;
     main.style.display = "none";
+    
 
+    salon = document.getElementById('salon');
+
+    imagenes = document.getElementsByClassName('imagen'); //es una array de elementos.
     piedra = document.getElementById('piedra');
     papel = document.getElementById('papel');
     tijera = document.getElementById('tijera');
@@ -63,11 +69,24 @@ function listenerGroup (){
         event.preventDefault();
         
         startGame();
+        
     });
+
+    for(let i = 0; i < imagenes.length; i++){
+    imagenes[i].addEventListener("mouseover", ()=>{
+        imagenes[i].style.transform = "scale(1.2)";
+    });
+    imagenes[i].addEventListener("mouseout", ()=>{
+        imagenes[i].style.transform = "scale(1)";
+    });
+    }
+
+
 }
 
 function startGame(){
     main.style.display = "flex";
+    salon.style.display = 'none';
 }
 
 //funcion activa el juego cuando hago el submit
