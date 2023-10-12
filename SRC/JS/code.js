eb2231f032bd869f57611dcee7daca81c19dbdea
Cristian.Quiceno <cristian.qprog@gmail.com>
2023-10-12 14:19:48 +0200

    estructurando JS

--- SRC/JS/code.js
+++ SRC/JS/code.js
@@ -1,1 +1,55 @@
 //tenog el autofocus puesto en html, lo añado con JS mejor?
+
+let activar;
+let submit;
+
+function elementAccess (){
+    submit = document.getElementById('submit');
+
+}
+
+function listenerGroup (){
+    activar.addEventListener("submit", ()=>{
+
+
+    })
+
+}
+//funcion activa el juego cuando hago el submit
+
+function startGame(){
+
+}
+
+function elementsModifier(){
+
+}
+
+function playerChoice(){
+    /** Crea un programa que permita 
+     * al jugador ingresar su elección: "piedra", "papel", "tijeras", “lagarto” o “Spock”.*/
+}
+
+function pcChoice(){
+    /*Genera aleatoriamente la elección del oponente (el ordenador) utilizando un número aleatorio.*/
+}
+
+function choiceResult (){
+    /**Compara la elección del jugador con la elección del oponente y determina 
+     * el resultado del juego según las siguientes reglas: reglas......*/
+}
+
+function winner( ){
+    /**Muestra el resultado del juego al jugador. */
+}
+
+function rePlay (){
+    /**Permite al jugador jugar nuevamente si lo desea. */
+}
+
+/*
+-UN fn que añada el nombre del player en la frase de elige: ¡player, elige sabiamente!
+*/ 
+window.addEventListener("load", ()=>{
+
+})
